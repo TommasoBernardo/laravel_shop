2f9236a3d066d4fd03d2c7cf9d414540d3b6fae3
TommasoBernardo <tommasob1210@gmail.com>
2023-04-17 11:59:00 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
+
 
 Route::get('/dashboard', [BookController::class, 'index']);
 Route::get('/shopping-cart', [BookController::class, 'bookCart'])->name('shopping.cart');
 Route::get('/book/{id}', [BookController::class, 'addBooktoCart'])->name('addbook.to.cart');
 Route::patch('/update-shopping-cart', [BookController::class, 'updateCart'])->name('update.sopping.cart');
 Route::delete('/delete-cart-product', [BookController::class, 'deleteProduct'])->name('delete.cart.product');
+require __DIR__ . '/auth.php';
