e618173b11f0f9813a7141064dbdac79954a19fe
TommasoBernardo <tommasob1210@gmail.com>
2023-04-17 12:13:39 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\BookController;
 */
 
 
-Route::get('/dashboard', [BookController::class, 'index']);
+Route::get('/', [BookController::class, 'index']);
 Route::get('/shopping-cart', [BookController::class, 'bookCart'])->name('shopping.cart');
 Route::get('/book/{id}', [BookController::class, 'addBooktoCart'])->name('addbook.to.cart');
 Route::patch('/update-shopping-cart', [BookController::class, 'updateCart'])->name('update.sopping.cart');
