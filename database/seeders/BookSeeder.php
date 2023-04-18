e01e32bf7f96471a893a79135c07ad0a819fc27c
TommasoBernardo <tommasob1210@gmail.com>
2023-04-18 16:40:25 +0200

    create seeder

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -45,6 +45,48 @@ class BookSeeder extends Seeder
                 'author' => 'Anna Bright',
                 'image' => 'https://bookcart.azurewebsites.net/Upload/5ba1968d-a7f5-4a04-99c2-281088b8532fqq.jpg',
                 'price' => 998
+            ],
+            [
+                'name' => 'La ladra di parole',
+                'author' => ' Abi Daré',
+                'image' => 'https://www.ibs.it/images/9788842932932_0_536_0_75.jpg',
+                'price' => 17.99
+            ],
+            [
+                'name' => 'Lo Hobbit o la riconquista del tesoro',
+                'author' => ' John R. R. Tolkien',
+                'image' => 'https://m.media-amazon.com/images/I/71gWo8CqMsL.jpg',
+                'price' => 20.00
+            ],
+            [
+                'name' => 'La ragazza di fuoco. Hunger games ',
+                'author' => ' Suzanne Collins ',
+                'image' => 'https://m.media-amazon.com/images/I/71-6LTR2ejL.jpg',
+                'price' => 7.99
+            ],
+            [
+                'name' => 'Il ladro di fulmini',
+                'author' => ' Rick Riordan',
+                'image' => 'https://www.nonsolocinema.com/wp/wp-content/uploads/2010/03/arton19721.jpg',
+                'price' => 9.88
+            ],
+            [
+                'name' => 'Gli arancini di Montalbano',
+                'author' => ' Andrea Camilleri',
+                'image' => 'https://www.mondadoristore.it/img/Commissario-Montalbano-Il-Alberto-Sironi/ea803280704576/BL/BL/10/NZO/?tit=Commissario+Montalbano+%28Il%29+-+Gli+Arancini+Di+Montalbano&aut=Alberto+Sironi',
+                'price' => 9
+            ],
+            [
+                'name' => 'The Witcher',
+                'author' => 'Andrzej Sapkowski',
+                'image' => 'https://www.lafeltrinelli.it/images/9781473231061_0_424_0_75.jpg',
+                'price' => 998
+            ],
+            [
+                'name' => 'Shadowhunters - 1. Città di ossa ',
+                'author' => 'Cassandra Clare ',
+                'image' => 'https://m.media-amazon.com/images/I/51fAUkwDo-L.jpg',
+                'price' => 12
             ]
         ];
 
